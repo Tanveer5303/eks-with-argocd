2cc33b9af4e38f1abb46d36525ac29ce1c609f22
Ali Syed Tanveer <cse.alisyedtanveer@gmail.com>
2025-08-07 21:02:30 +0530

    updated

--- modules/argocd/main.tf
+++ modules/argocd/main.tf
@@ -1,19 +1,20 @@
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = var.argocd_chart_version
-
-  create_namespace = true
-
-  values = [
-    file("${path.module}/values/argocd-values.yaml")
-  ]
-}
+#provider "helm" {
+#  kubernetes {
+#    config_path = "~/.kube/config"
+#  }
+#}
+#
+#resource "helm_release" "argocd" {
+#  name       = "argocd"
+#  namespace  = "argocd"
+#  repository = "https://argoproj.github.io/argo-helm"
+#  chart      = "argo-cd"
+#  version    = var.argocd_chart_version
+#
+#  create_namespace = true
+#
+#  values = [
+#    file("${path.module}/values/argocd-values.yaml")
+#  ]
+#}
+#
